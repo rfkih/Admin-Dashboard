1e690be14b401f3ce32338f9ec87c2be1d98470a
rfkih <rifkihidayat23@yahoo.com>
2022-06-06 13:04:06 +0700

    useRef success

--- src/pages/Tablebody.jsx
+++ src/pages/Tablebody.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React,{useState, useRef, useEffect} from 'react'
 
 import { Typography,Container, Grid, TextField, Card, Avatar, CardContent,InputBase, Input, IconButton,  OutlinedInput, InputAdornment,  FormControl, InputLabel, MenuItem, Select, CardActions, Button, Paper,Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, CardHeader } from '@material-ui/core'
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
@@ -6,6 +6,27 @@ import {Link} from 'react-router-dom'
 
 function Tablebody({item, columnsProducts}) {
     const [isEdit, setIsEdit] = useState(false)
+    const btnRef = useRef()
+
+
+
+    
+ useEffect(() => {
+    
+
+    const closeEdit = (e) => {
+        console.log(btnRef.current.contains(e.target));
+       if (!btnRef.current.contains(e.target)) {
+           setIsEdit(false)
+       }
+        
+    }
+   document.body.addEventListener('click', closeEdit)
+
+   return () => document.body.removeEventListener('click', closeEdit)
+ }, [])
+ 
+   
 
     const editFunction = () => (
         setIsEdit(!isEdit)
@@ -17,12 +38,12 @@ function Tablebody({item, columnsProducts}) {
     return (
         <>
             {isEdit ? 
-                  <TableRow hover role="checkbox" tabIndex={-1} key={item.id}>
+                  <TableRow  ref={btnRef} hover role="checkbox" tabIndex={-1} key={item.id}>
                     {columnsProducts.map((column) => {
                       const value = item[column.id];
                       if (column.id === "productIMG" ) {    
                         return (
-                            <TableCell key={column.id} align={column.align}  style={{ maxWidth:  100}}>
+                            <TableCell  key={column.id} align={column.align} >
                                 <div className='flex items-center'>
                                 <img
                                   className=' rounded-md h-20 w-24 mr-2'
@@ -34,14 +55,14 @@ function Tablebody({item, columnsProducts}) {
                         )  
                       } else if(column.id === "isLiquid") {
                         return (    
-                          <TableCell key={column.id} align={column.align}>
+                          <TableCell  key={column.id} align={column.align}>
                             {value ? <p className="font-semibold">Liquid</p> :  <p className="font-semibold" >Non-Liquid</p>}
                           </TableCell>                                      
                         )
                       } else if(column.id === "rownumber") {
                           return(
                               
-                              <TableCell key={column.id} align={column.align}>
+                              <TableCell  key={column.id} align={column.align}>
                                 {value}  
                               </TableCell> 
                           )      
